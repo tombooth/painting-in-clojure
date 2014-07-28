7ec112833eeb0191c26f9ef8710aba32fe376ddd
Tom Booth <tombooth@gmail.com>
2014-07-28 12:16:16 +0100

    Add JS files too

--- update_gh_pages.sh
+++ update_gh_pages.sh
@@ -2,14 +2,21 @@
 
 set -euo pipefail
 
-cat src/uk/co/tombooth/pollock.cljs | python tools/clojure-to-markdown.py > out.md
-pandoc --mathjax --template=tools/pandoc-template.html -s out.md -o index.html
+mkdir .working
+
+cat src/uk/co/tombooth/pollock.cljs | python tools/clojure-to-markdown.py > .working/out.md
+pandoc --mathjax --template=tools/pandoc-template.html -s .working/out.md -o .working/index.html
+cp -r web/js .working/
 
 git checkout gh-pages
 
+cp .working/index.html index.html
+cp -r .working/js .
+
 git add index.html
+git add js
 git commit -m "Update homepage" || true
 
 git checkout master
 
-rm out.md
+rm -rf .working
