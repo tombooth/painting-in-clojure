c5e4a50b7afa2bf14b73017b045448bd5f9c16d5
Tom Booth <tombooth@gmail.com>
2014-08-03 21:31:18 +0100

    Add links to clojuredocs in code snippets
    This sed expression will find any keywords and convert them to links
    that point to clojuredocs

--- update_gh_pages.sh
+++ update_gh_pages.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 mkdir .working
 
 cat src/uk/co/tombooth/pollock.cljs | python tools/clojure-to-markdown.py > .working/out.md
-pandoc --mathjax --template=tools/pandoc-template.html -s .working/out.md -o .working/index.html
+pandoc --mathjax --template=tools/pandoc-template.html -s .working/out.md | sed -E 's/<span class="kw">([^<]*)<\/span>/<a href="http:\/\/clojuredocs.org\/clojure_core\/clojure.core\/\1" target="_blank" class="kw">\1<\/a>/g' > .working/index.html
 cp -r web/js .working/
 
 git checkout gh-pages
